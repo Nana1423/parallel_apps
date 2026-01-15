6f9c4bb942e86c726b4591c8a37c379b835dcfe6
Nana1423 <emanuel.leite.dev@outlook.com>
2026-01-15 18:56:25 -0300

    - Reorganizing the files for dir - Adding e8-parallel.c

---
+++ OpenMP/e8-parallel.c
@@ -0,0 +1,23 @@
+/* Programa e8-serial.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+static long num_steps=100000000;
+double step, pi;
+int n_hilos = 6;
+
+void main() {
+	 int i; double x, sum = 0.0;
+	 step = 1.0/(double) num_steps;
+     omp_set_num_threads(n_hilos);
+	 
+     #pragma omp parallel for reduction(+:sum) private(x)
+	 for (i=0; i< num_steps; i++) {
+		 x = (i+0.5)*step;            // Variable privada x
+		 sum = sum + 4.0/(1.0 + x*x); // Variable compartida sum
+	 }
+	 
+	 pi = step * sum;
+	 printf("Pi = %f\n",pi);
+}
