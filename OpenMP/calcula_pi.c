d4e169d9306c11b1177f7b1a25e118761a6320b7
Nana1423 <emanuel.leite.dev@outlook.com>
2026-01-15 21:25:14 -0300

    fixing incorrect sign

--- OpenMP/calcula_pi.c
+++ OpenMP/calcula_pi.c
@@ -4,16 +4,15 @@
 double calcula_pi(double step) {
     double sum = 0.0;
 
-    // Abrimos la región paralela primero
     #pragma omp parallel 
     {
         double x;
-        double local_sum = 0.0; // IMPORTANTE: Cada hilo tiene su propia copia local
+        double local_sum = 0.0; 
 
         // Repartimos las iteraciones entre los hilos
         #pragma omp for private(x)
         for (int i = 0; i < 1000000; ++i) {
-            x = (i + 0.5) * step; // Usamos +0.5 para el punto medio estándar
+            x = (i - 0.5) * step; 
             local_sum += 4.0 / (1.0 + x * x);
         }
 
